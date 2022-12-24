beedf752975963d14be5a002a7b0a0dd52e17fc2
Kevin <kevindarmawan022@gmail.com>
2022-12-25 00:23:11 +0700

    l

--- src/main.js
+++ src/main.js
@@ -4,6 +4,5 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import Notifications from "@kyvg/vue3-notification";
-import axios from "axios";
 
 createApp(App).use(router).use(Notifications).mount("#app");
